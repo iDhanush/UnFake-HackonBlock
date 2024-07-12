70b0d28bed93d4eae3affe1e998b4b002ea8e964
iDhanush <idhanushm@gmail.com>
2024-07-12 23:59:33 +0530

    main and server

---
+++ backend/main.py
@@ -0,0 +1,17 @@
+import uvicorn
+from fastapi import FastAPI
+from starlette.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.mount("/dwd", StaticFiles(directory="assets"), name="download")
+uvicorn.run(app, host="0.0.0.0", port=8000)
