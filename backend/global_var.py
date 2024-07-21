aa490a2656d0096a448549c6ac3011557137d238
iDhanush <idhanushm@gmail.com>
2024-07-21 21:42:40 +0530

    database ini

---
+++ backend/global_var.py
@@ -0,0 +1,5 @@
+from backend.database import DataBase
+
+
+class Var:
+    db: DataBase = DataBase()
--- backend/server.py
+++ backend/server.py
@@ -1,3 +1,13 @@
 from fastapi import FastAPI
+from backend.global_var import Var
+from backend.database import DataBase
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(_fastapi: FastAPI):
+    Var.db = DataBase()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
