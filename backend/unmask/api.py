01c0711225d2769480c21e97e7cc2166f9b51f85
iDhanush <idhanushm@gmail.com>
2024-07-22 21:09:41 +0530

    unmask api

--- backend/unmask/api.py
+++ backend/unmask/api.py
@@ -3,9 +3,8 @@ from fastapi import APIRouter
 unmask_router = APIRouter(tags=['unmask'])
 
 
-@unmask_router.get('/unmask/<file_uid:str>')
-async def unmasker(file_uid: str):
-    file = open(f'assets/{file_uid}', 'rb')
-
+@unmask_router.get('/unmask/{client_address:str}/<file_uid:str>')
+async def unmasker(client_address: str, file_uid: str):
+    file = open(f'assets/{client_address}_{file_uid}', 'rb')
     return {
         "url": "https://static.vecteezy.com/system/resources/thumbnails/025/067/762/small_2x/4k-beautiful-colorful-abstract-wallpaper-photo.jpg"}
