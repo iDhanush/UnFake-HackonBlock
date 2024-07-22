9ecf25513d1f3fe9411ae32fc7d71d2c75f2669d
iDhanush <idhanushm@gmail.com>
2024-07-22 23:15:38 +0530

    blockchain api

--- backend/utils.py
+++ backend/utils.py
@@ -1,3 +1,5 @@
+import base64
+import hashlib
 import random
 import string
 
@@ -9,3 +11,24 @@ def invoke_uid(length=10, alphanumeric=True):
     char_pool = string.ascii_lowercase + string.ascii_uppercase if alphanumeric else string.digits
     uid = "".join(random.choices(char_pool, k=length))
     return uid
+
+
+def get_file(file_path):
+    with open(file_path, "rb") as file:
+        file = file.read()
+        return file
+
+
+def file_to_base64(file_path):
+    file_content = get_file(file_path)
+    base64_encoded = base64.b64encode(file_content)
+    base64_string = base64_encoded.decode('utf-8')
+    return base64_string
+
+
+def sha256(string):
+    return hashlib.sha256(string.encode()).hexdigest()
+
+
+def file_to_sha256(fid):
+    return sha256(file_to_base64(fid))
