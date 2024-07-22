dbc328a73c215581d9ec2660dd0a0d38a1ba510d
iDhanush <idhanushm@gmail.com>
2024-07-22 21:09:28 +0530

    upload with adress security

--- backend/uploads/api.py
+++ backend/uploads/api.py
@@ -1,19 +1,18 @@
 from fastapi import APIRouter, UploadFile, File, HTTPException
-
 from utils import invoke_uid, image_extensions, video_extensions
 
 upload_router = APIRouter(tags=['uploads'])
 
 
-@upload_router.post("/file/upload")
-async def upload_file(file: UploadFile = File(...)):
+@upload_router.post("/file/{client_address:str}/upload")
+async def upload_file(client_address: str, file: UploadFile = File(...)):
     fid = invoke_uid()
     print(file.filename)
     file_name = file.filename
     if file_name.split('.')[-1].lower() in image_extensions:
-        file_name = f'{fid}.jpg'
+        file_name = f'{client_address}_{fid}.jpg'
     elif file_name.split('.')[-1].lower() in video_extensions:
-        file_name = f'{fid}.mp4'
+        file_name = f'{client_address}_{fid}.mp4'
     else:
         raise HTTPException(401, '')
     with open(f'assets/{file_name}', "wb") as f:
