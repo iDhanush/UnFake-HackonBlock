8735a544071bef6f6d7ab00e1505030356cd459f
Abhinav K E <109943270+Abhinav-K-E@users.noreply.github.com>
2024-07-23 21:48:58 +0530

    ui fix

--- frontend/src/components/SpinLoader/SpinLoader.jsx
+++ frontend/src/components/SpinLoader/SpinLoader.jsx
@@ -5,7 +5,7 @@ const SpinLoader = () => {
   return (
     <div style={{
         width:'100%',
-        height:'100%',
+        height:'55vh',
         display:'flex',
         alignItems:'center',
         justifyContent:'center'
